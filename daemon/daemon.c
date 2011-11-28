5c373b2eab97c483c85a58d635e4868c830bcb36
Johannes Weiss <github@tux4u.de>
2011-11-28 18:49:06 +0100

    added license

--- daemon/daemon.c
+++ daemon/daemon.c
@@ -1,3 +1,22 @@
+/*
+ *  Records data from a NI USB-6218 and send it to connected clients
+ *
+ *  Copyright (C)2011, Johannes Weiß <weiss@tux4u.de>
+ *                   , Jonathan Dimond <jonny@dimond.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -351,3 +370,4 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+/* vim: set fileencoding=utf8 : */
