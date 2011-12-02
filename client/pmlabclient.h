b9ab8f2f9ac291661703b729b2af2a7de48d609b
Johannes Weiß <github@tux4u.de>
2011-12-02 21:39:51 +0100

    client: first drafts

---
+++ client/pmlabclient.h
@@ -0,0 +1,47 @@
+#ifndef PMLABCLIENT
+#define PMLABCLIENT
+
+void *pmlc_connect(char *server,
+                   unsigned int port,
+                   unsigned int *channels,
+                   unsigned int num_channels);
+int pmlc_read(void *handle,
+              size_t buffer_sizes,
+              double *analog_data,
+              digival_t *digital_data,
+              unsigned int *samples_read,
+              struct timespec *);
+void pmlc_close(void *handle);
+
+void pmlc_highlevel_read(void *handler,
+                         size_t buffer_size,
+                         double *analog_data,
+                         unsigned int data_channel,
+                         unsigned int trigger_channel,
+                         trigger_type ttype, /* embedded(+threshold), digital extern, separate channel(+threshold) */
+                         bool below,
+                         double threshold);
+
+double avs[1025];
+digival_t avs[1025];
+
+const unsigned int num_channels = 3;
+const unsigned int channels[] = { CPU1, TRIGGER1 };
+
+handle_t *h = pmlc_connect("localhost", 12345, channels, num_channels);
+double a_data[8192];
+digival_t d_data[8192];
+
+double *d;
+
+while(true) {
+    int samples_per_channel;
+    int ch_actually_read;
+    pmlc_read(h, 8192, a_data, d_data, &samples_per_channel, &ch_actually_read);
+    for (int j=0; j<num_channels; j++) {
+        for (int i=0; i<samples_per_channel; i++) {
+            printf("%f, %u\n", a_data[j*samples_per_channel+i], d_data[j][i]);
+        }
+    }
+}
+#endif
---
+++ common/conf.h
@@ -0,0 +1,26 @@
+#ifndef CONF_H
+#define CONF_H
+
+enum channel_ids {
+    CPU1 = 0,
+    TRIGGER1 = 1,
+    CPU2 = 2,
+    TRIGGER2 = 3,
+    CHAN5 = 4,
+    CHAN6 = 5,
+    CHAN7 = 6,
+    CHAN8 = 7
+};
+
+const char *ni_channels[] = {
+    "Dev1/ai0",
+    "Dev1/ai1",
+    "Dev1/ai2",
+    "Dev1/ai3",
+    "Dev1/ai4",
+    "Dev1/ai5",
+    "Dev1/ai6",
+    "Dev1/ai7"
+};
+
+#endif
