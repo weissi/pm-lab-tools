51c411d0f8e9bd476e7d50eef3e8896519d232c4
Johannes Weiß <github@tux4u.de>
2012-01-09 03:26:05 +0100

    checking_lab_client

---
+++ checking_lab_client.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+HERE=$(cd $(dirname "${BASH_SOURCE[0]}") > /dev/null && pwd)
+
+cd "$HERE"
+
+oldnum=0
+
+if [ ! -z $1 ]; then
+    SLEEP=$1
+else
+    SLEEP=10
+fi
+
+numstr="0"
+errs=0
+
+while read count num; do
+    numstr="$numstr,$num"
+    if [ $oldnum -ne 0 -a $(( $oldnum + 1 )) -ne $num ]; then
+        errs=$(( $errs + 1 ))
+        numstr="$numstr(XXX)"
+    fi
+    oldnum=$num
+done < <(
+    ( build/pmlabclient localhost 12345 2> /dev/null & PMC=$!;
+      sleep $SLEEP;
+      kill $PMC; wait ) 2> /dev/null | cut -d. -f1 |  uniq -c
+)
+
+if [ $errs -eq 0 ]; then
+    echo OK $numstr
+    exit 0
+else
+    echo "ERROR: $errs ($numstr)"
+    exit 1
+fi
