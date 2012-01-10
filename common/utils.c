9e911cb6f2c00e04e97e5e74840122bb5d4f2b22
Johannes Weiß <github@tux4u.de>
2012-01-10 20:49:36 +0100

    utils: fixed silly signature

--- common/utils.c
+++ common/utils.c
@@ -28,7 +28,7 @@ ssize_t full_write(int fd, const char *buf, size_t count) {
     return count;
 }
 
-ssize_t full_read(int fd, const char *buf, size_t count) {
+ssize_t full_read(int fd, char *buf, size_t count) {
     ssize_t res;
     size_t size = count;
 
--- common/utils.h
+++ common/utils.h
@@ -2,6 +2,6 @@
 #define UTILS_H
 
 ssize_t full_write(int fd, const char *buf, size_t count);
-ssize_t full_read(int fd, const char *buf, size_t count);
+ssize_t full_read(int fd, char *buf, size_t count);
 
 #endif
